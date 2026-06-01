b904b447109186bec9276341d6678049dc4aa1c6
pratibhacoding <pratibhaprajapati72272@gmail.com>
2026-06-02 02:27:05 +0530

    Hood design done

---
+++ www/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.text').textillate({
+        loop: true,
+        sync: true,
+        in: { effect: 'bounceIn',
+        },
+        out: { effect: 'bounceOut',
+        },
+    })
+
+});
