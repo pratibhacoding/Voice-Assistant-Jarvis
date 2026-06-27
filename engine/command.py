13454d1ee93e9dfd663f8c3fb464498e61ad9004
pratibhacoding <pratibhaprajapati72272@gmail.com>
2026-06-27 23:02:58 +0530

    voice change

--- engine/command.py
+++ engine/command.py
@@ -4,6 +4,7 @@ def speak(text):
    
     engine = pyttsx3.init('sapi5')
     voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[1].id)
     print(voices)
    
     engine.say(text)
