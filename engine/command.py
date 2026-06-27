8a3c58183cc96b3183bed5aa469957937856912e
pratibhacoding <pratibhaprajapati72272@gmail.com>
2026-06-27 23:05:08 +0530

    rate

--- engine/command.py
+++ engine/command.py
@@ -5,6 +5,7 @@ def speak(text):
     engine = pyttsx3.init('sapi5')
     voices = engine.getProperty('voices')
     engine.setProperty('voice', voices[0].id)
+    engine.setProperty('rate', 174) 
     print(voices)
    
     engine.say(text)
